73c03a44da645a2a5464d20e1d7dd1bbd38c00fd
Ajaykumar Kannan <kannan.ajay@gmail.com>
2012-07-19 21:09:14 +0530

    Changed to nanosleep

--- src/gnuanimation.cpp
+++ src/gnuanimation.cpp
@@ -5,7 +5,7 @@
  * ***************************************************/
 #include <iostream>
 #include <cmath>
-#include <unistd.h>
+#include <time.h>
 
 #define tab "\t"
 
@@ -13,13 +13,18 @@ using namespace std;
 
 int main(int argc,char **argv)
 {
+    struct timespec delay;
+    delay.tv_sec = 0;
+    delay.tv_nsec = 100000000L; 
     for(double t=0;t<=100*M_PI;t+=M_PI/512.0)
     {
         cout<<"p [-1.5:1.5][-1.5:1.5] '-' ps 2"<<endl;
         cout<<sin(t)<<tab<<cos(t)<<endl<<'e'<<endl<<flush;
         // the next line slows down the animation
         // for(int i=0;i<5000000;i++);
-	usleep(1000);
+	if (nanosleep(&delay, NULL)){
+	    return 1;
+	}
     }
     return 0;
 }
